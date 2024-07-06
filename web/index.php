2f243290d6d289772cd6f42430b6ef760b00d011
Puguh Setiyono <ankitpgh567@gmail.com>
2024-07-07 01:55:14 +0800

    praktikum docker

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            background-color: blue;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo "Nama saya : Puguh, saya belajar docker";
+    ?>
+</body>
+</html>
